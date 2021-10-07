ed1551a26444f4bbd89f2e7ac33a547d9b6068a1
YareliDanllely <yareli.rojas@ug.uchile.cl>
2021-10-06 22:08:17 -0300

    Se finalizo el avance de la tarea 2

--- src/main/java/interfaces/AttackableByBoo.java
+++ src/main/java/interfaces/AttackableByBoo.java
@@ -2,6 +2,10 @@ package interfaces;
 
 import character.BooEnemyCharacter;
 
+/**
+ * Se creo una interfaz que contiene los metodos de ataque de Boo
+ * junto con el metodo que calcula el daño que produce
+ */
 public interface AttackableByBoo {
 
    void calculateDamageFromBoo(BooEnemyCharacter Boo);
--- src/main/java/interfaces/AttackableByLuis.java
+++ src/main/java/interfaces/AttackableByLuis.java
@@ -2,7 +2,10 @@ package interfaces;
 
 import character.LuisMainCharacter;
 import character.MarcosMainCharacter;
-
+/**
+ * Se creo una interfaz que contiene los metodos de ataque de Luis
+ * junto con el metodo que calcula el daño que produce
+ */
 public interface AttackableByLuis {
 
     void calculateDamageFromLuisWhitHammer( LuisMainCharacter luis);
